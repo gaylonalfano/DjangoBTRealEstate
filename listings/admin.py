1f5b70acd62ef822e8f67e97b84b5a0dbef3794d
gaylonalfano <gaylon.alfano@gmail.com>
2018-11-04 21:51:02 +0800

    Added Listings and Realtors to admin area.

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+# Register your models here. Got the database up and going
+# and migrated the Listing and Realtor classes. Also created
+# a superuser account for admin. Now adding listings models to
+# admin area:
+admin.site.register(Listing)
