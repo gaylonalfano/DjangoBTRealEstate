8366eae2383ac24146d19ca161c3d9f6b63d8477
gaylonalfano <gaylon.alfano@gmail.com>
2018-11-06 11:22:20 +0800

    Configured admin area for Listings - display fields, link fields, filter area, checkbox, search, etc.

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Listing
 
-# Register your models here.
+# Documentation: https://docs.djangoproject.com/en/2.1/ref/contrib/admin/
 # THIS WAS SECOND: We've already customized some of the Admin CSS. Now
 # we want the Listings section within the Admin page to display more than
 # just the standard address (i.e., title field). Need to create a class:
@@ -13,6 +13,18 @@ class ListingAdmin(admin.ModelAdmin):
     # admin.site.register(Listing, ListingAdmin) below
     list_display = ('id', 'title', 'is_published',
                     'price', 'list_date', 'realtor')
+    # To enable another column field (i.e., "title") to be "clickable". Note that
+    # the default clickable column is the FIRST column (so "id"):
+    list_display_links = ('id', 'title')
+    # To add a filter box for your realtors:
+    list_filter = ('realtor',)  # Note the ","
+    # To enable is_published to be a clickable checkbox:
+    list_editable = ('is_published',)
+    # To enable searching:
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode', 'price')
+    # Define/limit the number of listings to display per page:
+    list_per_page = 25
 
 
 # THIS WAS FIRST: Got the database up and going
