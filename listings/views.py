053bb50d552560b2baa6375ec596f4062283563c
gaylonalfano <gaylon.alfano@gmail.com>
2018-11-06 13:39:54 +0800

    Configured our views.py in our listings app to pull data from our db. We then edited our listings.html file to loop through our listings (if any) and display to the site.

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+# Now e've setup the database, added some listings data, configured the admin area
+# **IMPORTANT** The idea here is to fetch our listings using our model and then we
+# insert it into our template, and then we can simply loop through and output the
+# listings that are in the database! In order to do that we need to bring in any
+# model we want into any file we want and use it! In this case we want to bring in
+# our Listing model using from .models import Listing
+from .models import Listing
 
 # Already created the paths for listings, listing, and search
 # They use views methods we need to create:
@@ -7,7 +14,13 @@ from django.shortcuts import render
 
 def index(request):
     # view we want to render
-    return render(request, 'listings/listings.html')
+    # fetch listings from database
+    listings = Listing.objects.all()
+    # create a dict to store listings we want to pass to the template
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request):
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Realtor
 
-# Register your models here.
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)  # Note the ","
+    list_per_page = 25
+
+
+# Don't forget to add/pass the new RealtorAdmin class:
+admin.site.register(Realtor, RealtorAdmin)
