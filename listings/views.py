a207fc9ebfc7c0bf99b046d5015ae05042f747ee
gaylonalfano <gaylon.alfano@gmail.com>
2018-11-10 15:22:17 +0800

    Added filters to our search queryset_list to only return listings based on search filters/criteria. Having some issues if I search bedrooms=2. Next going to work on preserving search criteria after submitting a search (currently refreshes).

--- listings/views.py
+++ listings/views.py
@@ -43,11 +43,53 @@ def listing(request, listing_id):
 
 def search(request):
     """Similar to our pages index method to handle the search form"""
+    # Build our search form queryset
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Building our filters
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(
+                city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(
+                state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
     # Add search form choices to context
     context = {
         'state_choices': state_choices,
         'price_choices': price_choices,
-        'bedroom_choices': bedroom_choices
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list,
+        # Adding 'values' to context to preserve search criteria
+        'values': request.GET
     }
 
     return render(request, 'listings/search.html', context)
