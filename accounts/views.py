6d91827c99961728991cb1737d76f8542c572a45
gaylonalfano <gaylon.alfano@gmail.com>
2018-11-11 12:03:39 +0800

    Updated our Register and Login templates using our markup/BS code and extending the base.html. Also added CSRF Token security to our forms. Started adding logic in our accounts views register and login view methods to determine if the request is a GET or a POST.

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render, redirect
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    # Determine if request is a GET or POST
+    if request.method == 'POST':  # A form submission
+        # Register User
+        pass
+    else:  # Just render the form
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Login User
+        pass
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
